23436951873ee5fa48c830d5934fa66817edf19c
sneha-kamra <snehakamra02@gmail.com>
2026-08-11 18:17:25 +0530

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("sql308.hstn.me", "mseet_41679233", "CareerPrep2026", "mseet_41679233_careerprepdb");
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+?>
